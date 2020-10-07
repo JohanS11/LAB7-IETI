9f36e16283f251cff4bc1cee0fc29a9ff711d3f2
Johan Arias <johan.arias@mail.escuelaing.edu.co>
2020-10-06 23:35:47 -0500

    front + functions working

--- functions/add-task/index.js
+++ functions/add-task/index.js
@@ -1,20 +1,26 @@
+var uniqid = require('uniqid');
+
+
+
 module.exports = async function (context, req) {
 
-
+    
     context.log('Adding a task to the planner');
    
     const task = req.body;
-    const id = uniqid(task.id);
-   
+    
     if (req.method == "GET"){
         context.res = {
             status: 200,
             body: exampletask
         };
     }else{
+        
+        exampletask.push(task);
+        task.id = uniqid("hola")
         context.res = {
             status: 200,
-            body: "hola"
+            body: exampletask
         };
     }
 
@@ -22,18 +28,19 @@ module.exports = async function (context, req) {
 }
 
 var exampletask = [{ 
+                    id : 123,
                     text:"Tarea1",
                     status:"Ready",
                     dueDate: new Date(),
                     responsible:'Johan'},
                    {
-                   
+                    id : 13233,
                     text:"Do back",
                     status:"Done",
                     dueDate:new Date(),
                     responsible:'Jeisson'},
                     {
-            
+                    id : 123213,
                     text: "Frontend",
                     status:"In progress",
                     dueDate:new Date(),
